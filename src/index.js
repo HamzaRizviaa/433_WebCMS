961f46abc9aa78fe455152c4b742f7b52eef1431
Umer Shaikh <umershaikh305@gmail.com>
2021-11-22 23:30:17 +0500

    Update styling

--- src/index.js
+++ src/index.js
@@ -1,16 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import './styles/index.scss';
 import { Provider } from 'react-redux';
 import store from './store/store';
-import { BrowserRouter as Router} from 'react-router-dom';
+import { BrowserRouter as Router } from 'react-router-dom';
 import AppRoutes from './routes';
 
 ReactDOM.render(
-  <Provider store={store}>
-    <Router>
-      <AppRoutes/>
-    </Router>
-  </Provider>,
-  document.getElementById('root')
+	<Provider store={store}>
+		<Router>
+			<AppRoutes />
+		</Router>
+	</Provider>,
+	document.getElementById('root')
 );
