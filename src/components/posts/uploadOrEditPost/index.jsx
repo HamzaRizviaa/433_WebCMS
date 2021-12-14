107e843afc1ae74f1b5890c40a6d475328dde5ff
Umer Shaikh <umershaikh305@gmail.com>
2021-12-15 01:03:11 +0500

    Implement media upload section

--- src/components/posts/uploadOrEditPost/index.jsx
+++ src/components/posts/uploadOrEditPost/index.jsx
@@ -9,12 +9,14 @@ import MenuIcon from '@material-ui/icons/Menu';
 import PropTypes from 'prop-types';
 import Slider from '../../slider';
 import { MenuItem, TextField, Select } from '@material-ui/core';
+import { CircularProgress } from '@material-ui/core';
 import ToggleSwitch from '../../switch';
 import Button from '../../button';
 import { useDispatch, useSelector } from 'react-redux';
 import { getMedia } from './mediaDropdownSlice';
 import { DragDropContext, Draggable, Droppable } from 'react-beautiful-dnd';
 import { makeid } from '../../../utils/helper';
+import axios from 'axios';
 
 const UploadOrEditPost = ({ open, handleClose }) => {
 	const [caption, setCaption] = useState('');
@@ -23,9 +25,12 @@ const UploadOrEditPost = ({ open, handleClose }) => {
 	const [mediaError, setMediaError] = useState('');
 	const [fileRejectionError, setFileRejectionError] = useState('');
 	const [uploadedFiles, setUploadedFiles] = useState([]);
+	const [filesUploadingStatus, setFilesUploadingStatus] = useState([]);
 	const [dropZoneBorder, setDropZoneBorder] = useState('#ffff00');
 	const [mediaLabelColor, setMediaLabelColor] = useState('#ffffff');
 	const [selectedMedia, setSelectedMedia] = useState(null);
+	// eslint-disable-next-line no-unused-vars
+	const [loadingMedia, setLoadingMedia] = useState([]);
 	const { acceptedFiles, fileRejections, getRootProps, getInputProps } =
 		useDropzone({
 			accept: 'image/jpeg, image/png, video/mp4',
@@ -57,30 +62,116 @@ const UploadOrEditPost = ({ open, handleClose }) => {
 		}
 	}, [fileRejections]);
 
+	const getFileType = (type) => {
+		if (type) {
+			let _type = type.split('/');
+			return _type && _type[1];
+		}
+	};
+
 	useEffect(() => {
 		if (acceptedFiles?.length) {
 			setUploadMediaError('');
 			setDropZoneBorder('#ffff00');
+			let newFilesUploadingStatus = [];
 			let newFiles = acceptedFiles.map((file) => {
+				let id = makeid(10);
+				newFilesUploadingStatus.push({
+					id,
+					uploadStatus: 'inprogress',
+					fileExtension: `.${getFileType(file.type)}`,
+					type: file.type,
+					file: file
+				});
+				setLoadingMedia((loadingMedia) => [...loadingMedia, id]);
 				if (file.type === 'video/mp4') {
 					return {
+						id: id,
 						fileName: file.name,
-						id: makeid(10),
 						img: URL.createObjectURL(file),
+						fileExtension: `.${getFileType(file.type)}`,
+						mime_type: file.type,
+						file: file,
 						type: 'video'
 					};
 				} else {
 					return {
+						id: id,
 						fileName: file.name,
-						id: makeid(10),
 						img: URL.createObjectURL(file),
+						fileExtension: `.${getFileType(file.type)}`,
+						mime_type: file.type,
+						file: file,
 						type: 'image'
 					};
 				}
 			});
+
+			setFilesUploadingStatus([
+				...filesUploadingStatus,
+				...newFilesUploadingStatus
+			]);
 			setUploadedFiles([...uploadedFiles, ...newFiles]);
 		}
 	}, [acceptedFiles]);
+
+	useEffect(() => {
+		if (uploadedFiles.length) {
+			uploadedFiles.map(async (uploadedFile) => {
+				if (loadingMedia.includes(uploadedFile.id)) {
+					try {
+						const result = await axios.post(
+							`https://vinuek3uf8.execute-api.us-east-1.amazonaws.com/dev/api/v1/post/get-signed-url`,
+							{
+								fileType: uploadedFile.fileExtension,
+								parts: 1
+							}
+						);
+
+						if (result?.data?.result?.url) {
+							const _result = await axios.put(
+								result?.data?.result?.url,
+								uploadedFile.file,
+								{
+									headers: { 'Content-Type': uploadedFile.mime_type }
+								}
+							);
+							if (_result?.status === 200) {
+								const uploadResult = await axios.post(
+									`https://vinuek3uf8.execute-api.us-east-1.amazonaws.com/dev/api/v1/post/complete-upload`,
+									{
+										file_name: uploadedFile.file.name,
+										data: {
+											Bucket: 'a',
+											MultipartUpload: ['a'],
+											Key: result?.data?.result?.Key,
+											UploadId: 'a'
+										}
+									}
+								);
+								if (uploadResult?.data?.status === 200) {
+									setLoadingMedia((_loadingMedia) =>
+										_loadingMedia.filter((media) => media != uploadedFile.id)
+									);
+								} else {
+									throw 'Error in uploading file';
+								}
+							} else {
+								throw 'Error in uploading file';
+							}
+						} else {
+							throw 'Error in uploading file';
+						}
+					} catch (error) {
+						setLoadingMedia((_loadingMedia) =>
+							_loadingMedia.filter((media) => media != uploadedFile.id)
+						);
+						console.log({ error });
+					}
+				}
+			});
+		}
+	}, [uploadedFiles]);
 
 	const resetState = () => {
 		setCaption('');
@@ -89,6 +180,7 @@ const UploadOrEditPost = ({ open, handleClose }) => {
 		setMediaError('');
 		setFileRejectionError('');
 		setUploadedFiles([]);
+		setFilesUploadingStatus([]);
 		setDropZoneBorder('#ffff00');
 		setMediaLabelColor('#ffffff');
 		setSelectedMedia(null);
@@ -142,7 +234,6 @@ const UploadOrEditPost = ({ open, handleClose }) => {
 	};
 
 	const postBtnDisabled = !uploadedFiles.length || (value && !selectedMedia);
-
 	return (
 		<Slider open={open} handleClose={handleClose} title={'Upload a Post'}>
 			<div className={classes.contentWrapper}>
@@ -162,7 +253,9 @@ const UploadOrEditPost = ({ open, handleClose }) => {
 												key={file.id}
 												draggableId={`droppable-${file.id}`}
 												index={index}
-												isDragDisabled={uploadedFiles.length <= 1}
+												isDragDisabled={
+													uploadedFiles.length <= 1 || loadingMedia.length > 0
+												}
 											>
 												{(provided) => (
 													<div
@@ -193,6 +286,15 @@ const UploadOrEditPost = ({ open, handleClose }) => {
 																{file.fileName}
 															</p>
 														</div>
+
+														{loadingMedia.includes(file.id) ? (
+															<div className={classes.loaderContainer}>
+																<CircularProgress className={classes.loader} />
+															</div>
+														) : (
+															<></>
+														)}
+
 														<div className={classes.filePreviewRight}>
 															<span {...provided.dragHandleProps}>
 																<MenuIcon
