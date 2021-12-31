9dbacaf492c82fb1ac89fd041a53d993ba428ecb
HamzaRizvi <hr04005@st.habib.edu.pk>
2021-12-31 18:33:36 +0500

    Media File Cover Image Title and Description

--- src/components/media/uploadOrEditMedia/index.jsx
+++ src/components/media/uploadOrEditMedia/index.jsx
@@ -4,13 +4,15 @@ import PropTypes from 'prop-types';
 import Slider from '../../slider';
 import Button from '../../button';
 import LoadingOverlay from 'react-loading-overlay';
-import { MenuItem, Select } from '@material-ui/core';
+import { MenuItem, TextField, Select } from '@material-ui/core';
 import KeyboardArrowDownIcon from '@material-ui/icons/KeyboardArrowDown';
 import { DragDropContext, Droppable } from 'react-beautiful-dnd';
 import DeleteIcon from '@material-ui/icons/Delete';
 import AddCircleOutlineIcon from '@material-ui/icons/AddCircleOutline';
 import { useDropzone } from 'react-dropzone';
 import { makeid } from '../../../utils/helper';
+
+import { ReactComponent as EyeIcon } from '../../../assets/Eye.svg';
 
 const UploadOrEditMedia = ({
 	open,
@@ -22,18 +24,36 @@ const UploadOrEditMedia = ({
 	const [mainCategory, setMainCategory] = useState('');
 	const [subCategory, setSubCategory] = useState('');
 	const [uploadedFiles, setUploadedFiles] = useState([]);
+	const [uploadedCoverImage, setUploadedCoverImage] = useState([]);
 	const [uploadMediaError, setUploadMediaError] = useState('');
 	const [dropZoneBorder, setDropZoneBorder] = useState('#ffff00');
 	const [fileRejectionError, setFileRejectionError] = useState('');
+	const [uploadCoverError, setUploadCoverError] = useState('');
+	const [dropZoneBorder2, setDropZoneBorder2] = useState('#ffff00');
+	const [fileRejectionError2, setFileRejectionError2] = useState('');
 	const [mainCategoryLabelColor, setMainCategoryLabelColor] =
 		useState('#ffffff');
 	const [mainCategoryError, setMainCategoryError] = useState('');
+	const [titleMedia, setTitleMedia] = useState('');
+	const [titleMediaLabelColor, setTitleMediaLabelColor] = useState('#ffffff');
+	const [titleMediaError, setTitleMediaError] = useState('');
+	const [description, setDescription] = useState('');
 
 	const { acceptedFiles, fileRejections, getRootProps, getInputProps } =
 		useDropzone({
 			accept: 'video/mp4',
 			maxFiles: 1
 		});
+
+	const {
+		acceptedFiles: acceptedFiles2,
+		fileRejections: fileRejections2,
+		getRootProps: getRootProps2,
+		getInputProps: getInputProps2
+	} = useDropzone({
+		accept: 'image/jpeg, image/png',
+		maxFiles: 1
+	});
 
 	useEffect(() => {
 		if (!open) {
@@ -49,6 +69,15 @@ const UploadOrEditMedia = ({
 			}, [5000]);
 		}
 	}, [fileRejections]);
+
+	useEffect(() => {
+		if (fileRejections2.length) {
+			setFileRejectionError2('The uploaded file format is not matching');
+			setTimeout(() => {
+				setFileRejectionError2('');
+			}, [5000]);
+		}
+	}, [fileRejections2]);
 
 	const getFileType = (type) => {
 		if (type) {
@@ -77,20 +106,54 @@ const UploadOrEditMedia = ({
 		}
 	}, [acceptedFiles]);
 
+	useEffect(() => {
+		if (acceptedFiles2?.length) {
+			setUploadCoverError('');
+			setDropZoneBorder2('#ffff00');
+			let newFiles = acceptedFiles2.map((file) => {
+				let id = makeid(10);
+				return {
+					id: id,
+					fileName: file.name,
+					img: URL.createObjectURL(file),
+					fileExtension: `.${getFileType(file.type)}`,
+					mime_type: file.type,
+					file: file,
+					type: file.type === 'video/mp4' ? 'video' : 'image'
+				};
+			});
+			setUploadedCoverImage([...uploadedCoverImage, ...newFiles]);
+		}
+	}, [acceptedFiles2]);
+
 	const resetState = () => {
 		setMainCategory('');
 		setSubCategory('');
 		setUploadedFiles([]);
+		setUploadedCoverImage([]);
 		setUploadMediaError('');
+		setUploadCoverError('');
 		setDropZoneBorder('#ffff00');
+		setDropZoneBorder2('#ffff00');
 		setFileRejectionError('');
+		setFileRejectionError2('');
 		setMainCategoryLabelColor('#ffffff');
+		setTitleMediaLabelColor('#ffffff');
+		setTitleMediaError('');
 		setMainCategoryError('');
+		setTitleMedia('');
+		setDescription('');
 	};
 
 	const handleDeleteFile = (id) => {
 		setUploadedFiles((uploadedFiles) =>
 			uploadedFiles.filter((file) => file.id !== id)
+		);
+	};
+
+	const handleDeleteFile2 = (id) => {
+		setUploadedCoverImage((uploadedCoverImage) =>
+			uploadedCoverImage.filter((file) => file.id !== id)
 		);
 	};
 
@@ -103,6 +166,14 @@ const UploadOrEditMedia = ({
 				setUploadMediaError('');
 			}, [5000]);
 		}
+		if (uploadedCoverImage.length < 1) {
+			setDropZoneBorder2('#ff355a');
+			setUploadCoverError('You need to upload a cover in order to post');
+			setTimeout(() => {
+				setDropZoneBorder2('#ffff00');
+				setUploadCoverError('');
+			}, [5000]);
+		}
 		if (!mainCategory) {
 			setMainCategoryLabelColor('#ff355a');
 			setMainCategoryError('You need to select main category');
@@ -111,9 +182,21 @@ const UploadOrEditMedia = ({
 				setMainCategoryError('');
 			}, [5000]);
 		}
+		if (!titleMedia) {
+			setTitleMediaLabelColor('#ff355a');
+			setTitleMediaError('You need to enter a Title');
+			setTimeout(() => {
+				setTitleMediaLabelColor('#ffffff');
+				setTitleMediaError('');
+			}, [5000]);
+		}
 	};
 
-	const addMediaBtnDisabled = !uploadedFiles.length || !mainCategory;
+	const addMediaBtnDisabled =
+		!uploadedFiles.length ||
+		!mainCategory ||
+		!uploadedCoverImage.length ||
+		!titleMedia;
 
 	return (
 		<Slider
@@ -271,12 +354,142 @@ const UploadOrEditMedia = ({
 										</div>
 									</section>
 								)}
+
+								<p className={classes.fileRejectionError}>
+									{fileRejectionError}
+								</p>
+
+								<h5>Add Cover Image</h5>
+								<DragDropContext>
+									<Droppable droppableId='droppable-2'>
+										{(provided) => (
+											<div
+												{...provided.droppableProps}
+												ref={provided.innerRef}
+												className={classes.uploadedFilesContainer}
+											>
+												{uploadedCoverImage.map((file, index) => {
+													return (
+														<div
+															key={index}
+															className={classes.filePreview}
+															ref={provided.innerRef}
+														>
+															<div className={classes.filePreviewLeft}>
+																{file.type === 'video' ? (
+																	<>
+																		<video
+																			id={'my-video'}
+																			//poster={isEdit ? file.img : null}
+																			className={classes.fileThumbnail}
+																			style={{ objectFit: 'cover' }}
+																		>
+																			<source src={file.img} />
+																		</video>
+																	</>
+																) : (
+																	<>
+																		<img
+																			src={file.img}
+																			className={classes.fileThumbnail}
+																		/>
+																	</>
+																)}
+
+																<p className={classes.fileName}>
+																	{file.fileName}
+																</p>
+															</div>
+
+															<div className={classes.filePreviewRight}>
+																<EyeIcon
+																	className={classes.filePreviewIcons}
+																	onClick={() => console.log('great')}
+																/>
+																<DeleteIcon
+																	className={classes.filePreviewIcons}
+																	onClick={() => {
+																		handleDeleteFile2(file.id);
+																	}}
+																/>
+															</div>
+														</div>
+													);
+												})}
+												{provided.placeholder}
+											</div>
+										)}
+									</Droppable>
+								</DragDropContext>
+								{!uploadedCoverImage.length && (
+									<section
+										className={classes.dropZoneContainer}
+										style={{
+											borderColor: dropZoneBorder2
+										}}
+									>
+										<div {...getRootProps2({ className: classes.dropzone })}>
+											<input {...getInputProps2()} />
+											<AddCircleOutlineIcon className={classes.addFilesIcon} />
+											<p className={classes.dragMsg}>
+												Click or drag files to this area to upload
+											</p>
+											<p className={classes.formatMsg}>
+												Supported formats are jpeg,png
+											</p>
+											<p className={classes.uploadMediaError}>
+												{uploadCoverError}
+											</p>
+										</div>
+									</section>
+								)}
+
+								<p className={classes.fileRejectionError}>
+									{fileRejectionError2}
+								</p>
+
+								<div className={classes.titleContainer}>
+									<h6 style={{ color: titleMediaLabelColor }}>TITLE</h6>
+									<TextField
+										value={titleMedia}
+										onChange={(e) => {
+											setTitleMedia(e.target.value);
+											setTitleMediaError('');
+											setTitleMediaLabelColor('#ffffff');
+										}}
+										placeholder={'Please write your title here'}
+										className={classes.textField}
+										InputProps={{
+											disableUnderline: true,
+											className: classes.textFieldInput
+										}}
+										multiline
+										maxRows={2}
+									/>
+								</div>
+								<p className={classes.mediaError}>{titleMediaError}</p>
+
+								<div className={classes.titleContainer}>
+									<h6>DESCRIPTION</h6>
+									<TextField
+										value={description}
+										onChange={(e) => {
+											setDescription(e.target.value);
+										}}
+										placeholder={'Please write your description here'}
+										className={classes.textField}
+										InputProps={{
+											disableUnderline: true,
+											className: classes.textFieldInput
+										}}
+										multiline
+										maxRows={4}
+									/>
+								</div>
 							</>
 						) : (
 							<></>
 						)}
-
-						<p className={classes.fileRejectionError}>{fileRejectionError}</p>
 					</div>
 					<div className={classes.buttonDiv}>
 						<div className={classes.addMediaBtn}>
