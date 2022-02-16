9fe38c55460ccbd61f543667905433f32d81c9cb
HamzaRizvi <hr04005@st.habib.edu.pk>
2022-02-16 15:25:45 +0500

    banners button

--- src/components/banners/Banners.jsx
+++ src/components/banners/Banners.jsx
@@ -8,6 +8,8 @@ import classes from './_banners.module.scss';
 // import { DragDropContext, Draggable, Droppable } from 'react-beautiful-dnd';
 import { ReactComponent as DropdownArrow } from '../../assets/drop_drown_arrow.svg';
 import { ReactComponent as Union } from '../../assets/Music.svg';
+import Button from '../button';
+
 const useStyles = makeStyles(() => ({
 	'&.Mui-focused .MuiOutlinedInput-notchedOutline': {
 		border: 'none',
@@ -38,100 +40,110 @@ export default function Banners() {
 	];
 
 	return (
-		<div className={classes.Banner}>
-			{bannersData.length > 0 &&
-				bannersData.map((data, index) => {
-					return (
-						<div className={classes.bannerRow} key={index}>
-							<div className={classes.bannertext}>Banner {data.id}</div>
-							<div className={classes.bannerRight}>
-								<div className={classes.dragIcon}>
-									<Union />
-								</div>
-								<div className={classes.bannerDropdownDiv}>
-									<label className={classes.bannerLabel}>
-										select Banner Type
-									</label>
-
-									<div className={classes.bannerDropdown}>
-										<Select
-											className={classUseStyle.bannerSelect}
-											onOpen={() => {
-												setDisableDropdown(false);
-											}}
-											onClose={() => {
-												setDisableDropdown(true);
-											}}
-											disabled={false}
-											value={bannerType}
-											onChange={(e) => {
-												setDisableDropdown(true);
-												setBannerType(e.target.value);
-											}}
-											// className={`${classes.select}`}
-											disableUnderline={true}
-											IconComponent={() => (
-												<DropdownArrow className={classes.dropdownicon} />
-											)}
-											MenuProps={{
-												anchorOrigin: {
-													vertical: 'bottom',
-													horizontal: 'left'
-												},
-												transformOrigin: {
-													vertical: 'top',
-													horizontal: 'left'
-												},
-												getContentAnchorEl: null
-											}}
-											displayEmpty={true}
-											renderValue={(value) =>
-												value?.length
-													? Array.isArray(value)
-														? value.join(', ')
-														: value
-													: ' '
-											}
-										>
-											{allMedia.length > 0 &&
-												allMedia.map((category, index) => {
-													return (
-														<MenuItem key={index} value={category}>
-															{category}
-														</MenuItem>
-													);
-												})}
-										</Select>
+		<>
+			<div className={classes.Banner}>
+				{bannersData.length > 0 &&
+					bannersData.map((data, index) => {
+						return (
+							<div className={classes.bannerRow} key={index}>
+								<div className={classes.bannertext}>Banner {data.id}</div>
+								<div className={classes.bannerRight}>
+									<div className={classes.dragIcon}>
+										<Union />
 									</div>
-								</div>
-
-								{bannerType == 'Please Select' ? (
-									''
-								) : (
 									<div className={classes.bannerDropdownDiv}>
-										<label
-											style={
-												selectedValue === ''
-													? { color: '#FF355A' }
-													: { color: 'white' }
-											}
-											className={classes.bannerLabel}
-										>
-											SELECT CONTENT
+										<label className={classes.bannerLabel}>
+											select Banner Type
 										</label>
 
 										<div className={classes.bannerDropdown}>
-											<Select>
-												<MenuItem>abc</MenuItem>
+											<Select
+												className={classUseStyle.bannerSelect}
+												onOpen={() => {
+													setDisableDropdown(false);
+												}}
+												onClose={() => {
+													setDisableDropdown(true);
+												}}
+												disabled={false}
+												value={bannerType}
+												onChange={(e) => {
+													setDisableDropdown(true);
+													setBannerType(e.target.value);
+												}}
+												// className={`${classes.select}`}
+												disableUnderline={true}
+												IconComponent={() => (
+													<DropdownArrow className={classes.dropdownicon} />
+												)}
+												MenuProps={{
+													anchorOrigin: {
+														vertical: 'bottom',
+														horizontal: 'left'
+													},
+													transformOrigin: {
+														vertical: 'top',
+														horizontal: 'left'
+													},
+													getContentAnchorEl: null
+												}}
+												displayEmpty={true}
+												renderValue={(value) =>
+													value?.length
+														? Array.isArray(value)
+															? value.join(', ')
+															: value
+														: ' '
+												}
+											>
+												{allMedia.length > 0 &&
+													allMedia.map((category, index) => {
+														return (
+															<MenuItem key={index} value={category}>
+																{category}
+															</MenuItem>
+														);
+													})}
 											</Select>
-											<DropdownArrow className={classes.dropdownicon} />
 										</div>
 									</div>
-								)}
+
+									{bannerType == 'Please Select' ? (
+										''
+									) : (
+										<div className={classes.bannerDropdownDiv}>
+											<label
+												style={
+													selectedValue === ''
+														? { color: '#FF355A' }
+														: { color: 'white' }
+												}
+												className={classes.bannerLabel}
+											>
+												SELECT CONTENT
+											</label>
+
+											<div className={classes.bannerDropdown}>
+												<Select>
+													<MenuItem>abc</MenuItem>
+												</Select>
+												<DropdownArrow className={classes.dropdownicon} />
+											</div>
+										</div>
+									)}
+								</div>
 							</div>
-						</div>
-					);
-				})}
-		</div>
+						);
+					})}
+			</div>
+
+			<div className={classes.buttonDiv}>
+				<Button
+					disabled={false}
+					onClick={() => {}}
+					text={'PUBLISH HOME BANNERS'}
+				/>
+			</div>
+		</>
 	);
 }
