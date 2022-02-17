8fd796435b540b92c013a9b4ad20a6c15281ee96
KhadijaHussain3212 <khadija.hussain@cloudprimero.com>
2022-02-17 14:37:07 +0500

    banner rows

---
+++ src/components/banners/BannerRows.jsx
@@ -0,0 +1,279 @@
+import React, { useState, useEffect } from 'react';
+// import Backdrop from '@material-ui/core/Backdrop';
+//import { makeStyles } from '@material-ui/core/styles';
+import { TextField } from '@material-ui/core';
+import MenuItem from '@mui/material/MenuItem';
+import Select from '@mui/material/Select';
+import classes from './_banners.module.scss';
+import Autocomplete from '@mui/material/Autocomplete';
+import ClearIcon from '@material-ui/icons/Clear';
+import { Popper, Paper } from '@mui/material';
+import { Draggable } from 'react-beautiful-dnd';
+//import KeyboardArrowDownIcon from '@material-ui/icons/KeyboardArrowDown';
+import PropTypes from 'prop-types';
+import { ReactComponent as DropdownArrow } from '../../assets/drop_drown_arrow.svg';
+import { ReactComponent as Union } from '../../assets/drag.svg';
+import { useStyles } from './bannerStyles';
+
+// const useStyles = makeStyles(() => ({
+// 	'&.Mui-focused': {
+// 		border: 'none !important',
+// 		borderRadius: 'none !important',
+// 		outline: 'none !important'
+// 	}
+// }));
+
+export default function BannerRows({ key, data }) {
+	// const listElement = useRef(null);
+	//styles
+	const muiClasses = useStyles();
+	//states
+	const [bannerType, setBannerType] = useState('');
+	//const classUseStyle = useStyles();
+	const [disableDropdown, setDisableDropdown] = useState(true);
+	const [dropdownPosition, setDropdownPosition] = useState(false);
+	const [selectedMedia, setSelectedMedia] = useState(null);
+	const [selectMediaInput, setSelectMediaInput] = useState('');
+	const allMedia = ['Title only', 'Title + Text', 'Please Select'];
+	//content type dropdown
+	const options = [
+		{
+			id: 1,
+			name: 'neymar'
+		},
+		{
+			id: 2,
+			name: 'salah'
+		},
+		{
+			id: 3,
+			name: 'kagawa'
+		},
+		{
+			id: 4,
+			name: 'Cristiano'
+		},
+		{
+			id: 5,
+			name: 'RONAAAAAAAAALD'
+		}
+	];
+
+	// a little function to help us with reordering the result
+
+	useEffect(() => {
+		console.log(data, 'provided');
+		if (!open) {
+			resetState();
+		}
+	}, [open]);
+	const resetState = () => {
+		setDropdownPosition(false);
+	};
+	const handleChangeSelectMediaInput = (e) => {
+		console.log(disableDropdown);
+		setSelectMediaInput(e.target.value);
+	};
+
+	// const removeClass = () => {
+	// 	// var element = document.getElementById('myDIV');
+	// 	if (listElement === undefined) {
+	// 		return;
+	// 	}
+
+	// 	listElement.classList.remove('Mui-focused');
+	// };
+
+	return (
+		<Draggable
+			key={data}
+			draggableId={`droppable-${data}`}
+			index={key}
+			//	isDragDisabled={uploadeddatas.length <= 1}
+		>
+			{(provided) => (
+				<div
+					key={key}
+					ref={provided.innerRef}
+					{...provided.draggableProps}
+					style={{
+						...provided.draggableProps.style
+					}}
+				>
+					<div className={classes.bannerRight} key={data} index={key}>
+						<div className={classes.dragIcon}>
+							<span {...provided.dragHandleProps}>
+								<Union
+									style={{ cursor: 'grab' }}
+									// className={classes.dataPreviewIcons}
+								/>
+							</span>
+						</div>
+						<div className={classes.bannerDropdownDiv}>
+							<label className={classes.bannerLabel}>select Banner Type</label>
+
+							<div className={classes.bannerDropdown}>
+								<Select
+									// className={classUseStyle.bannerSelect}
+									// ref={listElement}
+									// onClick={() => {
+									// 	removeClass();
+									// }}
+									onOpen={() => {
+										setDisableDropdown(false);
+									}}
+									onClose={() => {
+										setDisableDropdown(true);
+									}}
+									disabled={false}
+									value={bannerType}
+									onChange={(e) => {
+										setDisableDropdown(true);
+										setBannerType(e.target.value);
+									}}
+									disableUnderline={true}
+									IconComponent={(props) => (
+										<DropdownArrow
+											{...props}
+											className={classes.dropdownicon}
+										/>
+									)}
+									MenuProps={{
+										anchorOrigin: {
+											vertical: 'bottom',
+											horizontal: 'left'
+										},
+										transformOrigin: {
+											vertical: 'top',
+											horizontal: 'left'
+										},
+										getContentAnchorEl: null
+									}}
+									displayEmpty={true}
+									renderValue={(value) =>
+										value?.length
+											? Array.isArray(value)
+												? value.join(', ')
+												: value
+											: ' '
+									}
+								>
+									{allMedia.length > 0 &&
+										allMedia.map((category, index) => {
+											return (
+												<MenuItem
+													key={index}
+													value={category}
+													className={muiClasses.root}
+												>
+													{category}
+												</MenuItem>
+											);
+										})}
+								</Select>
+							</div>
+						</div>
+
+						{bannerType == 'Please Select' ? (
+							<div className={classes.bannerAutocomplete}></div>
+						) : (
+							<div
+								className={classes.bannerAutocomplete}
+								style={{
+									marginBottom: dropdownPosition ? 0 : 0
+								}}
+							>
+								<label className={classes.bannerLabel}>
+									select Banner Type
+								</label>
+								<Autocomplete
+									value={selectedMedia}
+									PaperComponent={(props) => {
+										setDisableDropdown(false);
+
+										return (
+											<Paper
+												elevation={6}
+												className={classes.popperAuto}
+												{...props}
+											/>
+										);
+									}}
+									PopperComponent={({ style, ...props }) => (
+										<Popper {...props} style={{ ...style, height: 0 }} />
+									)}
+									ListboxProps={{
+										style: { maxHeight: 180 },
+										position: 'bottom'
+									}}
+									onOpen={() => {
+										setDropdownPosition(true);
+									}}
+									onClose={() => {
+										setDisableDropdown(true);
+										setDropdownPosition(false);
+									}}
+									onChange={(e, newVal) => {
+										setSelectedMedia(newVal);
+										setDisableDropdown(true);
+									}}
+									options={options}
+									getOptionLabel={(option) => option.name}
+									// getOptionLabel={(option) => option.name}
+									renderOption={(props, option) => {
+										return (
+											<li {...props} className={classes.liAutocomplete}>
+												{option.name}
+											</li>
+										);
+									}}
+									// filterOptions={(items) => {
+									// 	return items.filter((item) =>
+									// 		item.title
+									// 			.toLowerCase()
+									// 			.includes(
+									// 				selectMediaInput.toLowerCase()
+									// 			)
+									// 	);
+									// }}
+									renderInput={(params) => (
+										<TextField
+											{...params}
+											size='small'
+											placeholder=''
+											InputProps={{
+												disableUnderline: true,
+												...params.InputProps,
+												className: classes.textFieldInput
+											}}
+											value={selectMediaInput}
+											onChange={handleChangeSelectMediaInput}
+										/>
+									)}
+									clearIcon={<ClearIcon />}
+									noOptionsText={
+										<div
+											style={{
+												color: '#808080',
+												fontSize: 14
+											}}
+										>
+											No Results Found
+										</div>
+									}
+									popupIcon={''}
+								/>
+							</div>
+						)}
+					</div>
+				</div>
+			)}
+		</Draggable>
+	);
+}
+
+BannerRows.propTypes = {
+	data: PropTypes.integer,
+	key: PropTypes.integer,
+	provided: PropTypes.draggableProps
+};
--- src/components/banners/Banners.jsx
+++ src/components/banners/Banners.jsx
@@ -1,80 +1,28 @@
-import React, { useState, useEffect } from 'react';
-// import Backdrop from '@material-ui/core/Backdrop';
-import { makeStyles } from '@material-ui/core/styles';
-import { TextField } from '@material-ui/core';
-import MenuItem from '@mui/material/MenuItem';
-import Select from '@mui/material/Select';
+import React, { useState } from 'react';
 import classes from './_banners.module.scss';
-import Autocomplete from '@mui/material/Autocomplete';
-import ClearIcon from '@material-ui/icons/Clear';
-import { Popper, Paper } from '@mui/material';
-import { DragDropContext, Draggable, Droppable } from 'react-beautiful-dnd';
-//import KeyboardArrowDownIcon from '@material-ui/icons/KeyboardArrowDown';
-
-import { ReactComponent as DropdownArrow } from '../../assets/drop_drown_arrow.svg';
-import { ReactComponent as Union } from '../../assets/drag.svg';
-
+import { DragDropContext, Droppable } from 'react-beautiful-dnd';
+import BannerRows from './BannerRows';
 import Button from '../button';
 
-const useStyles = makeStyles(() => ({
-	'&.Mui-focused': {
-		border: 'none !important',
-		borderRadius: 'none !important',
-		outline: 'none !important'
-	}
-}));
 export default function Banners() {
-	const classUseStyle = useStyles();
-	const [disableDropdown, setDisableDropdown] = useState(true);
-	const [dropdownPosition, setDropdownPosition] = useState(false);
-	const [selectedMedia, setSelectedMedia] = useState(null);
-	const [selectMediaInput, setSelectMediaInput] = useState('');
-	const options = [
-		{
-			id: 1,
-			name: 'neymar'
-		},
-		{
-			id: 2,
-			name: 'salah'
-		},
-		{
-			id: 3,
-			name: 'kagawa'
-		},
-		{
-			id: 4,
-			name: 'Cristiano'
-		},
-		{
-			id: 5,
-			name: 'RONAAAAAAAAALD'
-		}
-	];
 	const [bannerData, setBannerData] = useState([
 		{
-			id: '1',
-			name: 'neymar'
+			id: '1'
 		},
 		{
-			id: '2',
-			name: 'salah'
+			id: '2'
 		},
 		{
-			id: '3',
-			name: 'neymar'
+			id: '3'
 		},
 		{
-			id: '4',
-			name: 'neymar'
+			id: '4'
 		},
 		{
-			id: '5',
-			name: 'messi'
+			id: '5'
 		}
 	]);
-	const [bannerType, setBannerType] = useState('');
-	const allMedia = ['Title only', 'Title + Text', 'Please Select'];
+
 	const bannersDataValue = [
 		{
 			id: '1'
@@ -93,20 +41,18 @@ export default function Banners() {
 		}
 	];
 
-	// a little function to help us with reordering the result - autocomplete
 	const reorder = (list, startIndex, endIndex) => {
 		const result = Array.from(list);
 		const [removed] = result.splice(startIndex, 1);
 		result.splice(endIndex, 0, removed);
 		return result;
 	};
-	// - autocomplete
+
 	const onDragEnd = (result) => {
 		// dropped outside the list
 		if (!result.destination) {
 			return;
 		}
-
 		const items = reorder(
 			bannerData,
 			result.source.index, // pick
@@ -116,18 +62,6 @@ export default function Banners() {
 		setBannerData(items);
 	};
 	// - autocomplete ends
-
-	useEffect(() => {
-		if (!open) {
-			resetState();
-		}
-	}, [open]);
-	const resetState = () => {
-		setDropdownPosition(false);
-	};
-	const handleChangeSelectMediaInput = (e) => {
-		setSelectMediaInput(e.target.value);
-	};
 
 	return (
 		<div className={classes.Banner}>
@@ -142,218 +76,21 @@ export default function Banners() {
 							);
 						})}
 				</div>
-				<div className={classes.dragbale} disableDropdown={disableDropdown}>
+				<div
+					className={classes.dragbale}
+					//disableDropdown={disableDropdown}
+				>
 					<DragDropContext onDragEnd={onDragEnd}>
 						<Droppable droppableId='droppable-1'>
 							{(provided) => (
-								<div
-									{...provided.droppableProps}
-									ref={provided.innerRef}
-									// className={classes.uploadedFilesContainer}
-								>
-									{bannerData.map((file, index) => {
+								<div {...provided.droppableProps} ref={provided.innerRef}>
+									{bannerData.map((data, index) => {
 										return (
-											<Draggable
-												key={file.id}
-												draggableId={`droppable-${file.id}`}
-												index={index}
-
-												//	isDragDisabled={uploadedFiles.length <= 1}
-											>
-												{(provided) => (
-													<div
-														key={index}
-														ref={provided.innerRef}
-														{...provided.draggableProps}
-														style={{
-															...provided.draggableProps.style
-														}}
-													>
-														<div
-															className={classes.bannerRight}
-															key={file.id}
-															index={index}
-														>
-															<div className={classes.dragIcon}>
-																<span {...provided.dragHandleProps}>
-																	<Union
-																		style={{ cursor: 'grab' }}
-																		// className={classes.filePreviewIcons}
-																	/>
-																</span>
-															</div>
-															<div className={classes.bannerDropdownDiv}>
-																<label className={classes.bannerLabel}>
-																	select Banner Type
-																</label>
-
-																<div className={classes.bannerDropdown}>
-																	<Select
-																		className={classUseStyle.bannerSelect}
-																		onOpen={() => {
-																			setDisableDropdown(false);
-																		}}
-																		onClose={() => {
-																			setDisableDropdown(true);
-																		}}
-																		disabled={false}
-																		value={bannerType}
-																		onChange={(e) => {
-																			setDisableDropdown(true);
-																			setBannerType(e.target.value);
-																		}}
-																		//className={`${classes.select}`}
-																		disableUnderline={true}
-																		IconComponent={(props) => (
-																			<DropdownArrow
-																				{...props}
-																				className={classes.dropdownicon}
-																			/>
-																		)}
-																		MenuProps={{
-																			anchorOrigin: {
-																				vertical: 'bottom',
-																				horizontal: 'left'
-																			},
-																			transformOrigin: {
-																				vertical: 'top',
-																				horizontal: 'left'
-																			},
-																			getContentAnchorEl: null
-																		}}
-																		displayEmpty={true}
-																		renderValue={(value) =>
-																			value?.length
-																				? Array.isArray(value)
-																					? value.join(', ')
-																					: value
-																				: ' '
-																		}
-																	>
-																		{allMedia.length > 0 &&
-																			allMedia.map((category, index) => {
-																				return (
-																					<MenuItem
-																						key={index}
-																						value={category}
-																					>
-																						{category}
-																					</MenuItem>
-																				);
-																			})}
-																	</Select>
-																</div>
-															</div>
-
-															{bannerType == 'Please Select' ? (
-																''
-															) : (
-																<div
-																	className={classes.bannerAutocomplete}
-																	style={{
-																		marginBottom: dropdownPosition ? 0 : 0
-																	}}
-																>
-																	<label className={classes.bannerLabel}>
-																		select Banner Type
-																	</label>
-																	<Autocomplete
-																		value={selectedMedia}
-																		PaperComponent={(props) => {
-																			setDisableDropdown(false);
-
-																			return (
-																				<Paper
-																					elevation={6}
-																					className={classes.popperAuto}
-																					style={{
-																						marginTop: '12px',
-																						background: 'black',
-																						border: '1px solid #404040',
-																						boxShadow:
-																							'0px 16px 40px rgba(255, 255, 255, 0.16)',
-																						borderRadius: '8px'
-																					}}
-																					{...props}
-																				/>
-																			);
-																		}}
-																		PopperComponent={({ style, ...props }) => (
-																			<Popper
-																				{...props}
-																				style={{ ...style, height: 0 }}
-																			/>
-																		)}
-																		ListboxProps={{
-																			style: { maxHeight: 180 },
-																			position: 'bottom'
-																		}}
-																		onOpen={() => {
-																			setDropdownPosition(true);
-																		}}
-																		onClose={() => {
-																			setDisableDropdown(true);
-																			setDropdownPosition(false);
-																		}}
-																		onChange={(e, newVal) => {
-																			setSelectedMedia(newVal);
-																			setDisableDropdown(true);
-																		}}
-																		options={options}
-																		getOptionLabel={(option) => option.name}
-																		// getOptionLabel={(option) => option.name}
-																		renderOption={(props, option) => {
-																			return (
-																				<li
-																					{...props}
-																					className={classes.liAutocomplete}
-																				>
-																					{option.name}
-																				</li>
-																			);
-																		}}
-																		// filterOptions={(items) => {
-																		// 	return items.filter((item) =>
-																		// 		item.title
-																		// 			.toLowerCase()
-																		// 			.includes(
-																		// 				selectMediaInput.toLowerCase()
-																		// 			)
-																		// 	);
-																		// }}
-																		renderInput={(params) => (
-																			<TextField
-																				{...params}
-																				size='small'
-																				placeholder=''
-																				InputProps={{
-																					disableUnderline: true,
-																					...params.InputProps,
-																					className: classes.textFieldInput
-																				}}
-																				value={selectMediaInput}
-																				onChange={handleChangeSelectMediaInput}
-																			/>
-																		)}
-																		clearIcon={<ClearIcon />}
-																		noOptionsText={
-																			<div
-																				style={{
-																					color: '#808080',
-																					fontSize: 14
-																				}}
-																			>
-																				No Results Found
-																			</div>
-																		}
-																		popupIcon={''}
-																	/>
-																</div>
-															)}
-														</div>
-													</div>
-												)}
-											</Draggable>
+											<BannerRows
+												data={data.id}
+												key={index}
+												provided={provided}
+											/>
 										);
 									})}
 									{provided.placeholder}
