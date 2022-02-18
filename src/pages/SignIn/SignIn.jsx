e878c43d8303945c8640986cfad60c070d0f07a0
HamzaRizvi <hr04005@st.habib.edu.pk>
2022-02-18 23:21:05 +0500

    Sign In page folder structure

---
+++ src/pages/SignIn/SignIn.jsx
@@ -0,0 +1,8 @@
+//import React from 'react';
+//import classes from './_signIn.module.scss';
+
+const SignIn = () => {
+	return 'haha';
+};
+
+export default SignIn;
--- src/routes/index.js
+++ src/routes/index.js
@@ -4,11 +4,13 @@ import PostLibrary from '../pages/PostLibrary/PostLibrary';
 import MediaLibrary from '../pages/MediaLibrary/MediaLibrary';
 import QuizLibrary from '../pages/QuizLibrary/QuizLibrary';
 import TopBanner from '../pages/TopBanner/TopBanner.jsx';
+import SignIn from '../pages/SignIn/SignIn';
 
 const AppRoutes = () => {
 	return (
 		<>
 			<Routes>
+				<Route path='/sign-in' element={<SignIn />} />
 				<Route path='/post-library' element={<PostLibrary />} />
 				<Route path='/media-library' element={<MediaLibrary />} />
 				<Route path='/quiz-library' element={<QuizLibrary />} />
