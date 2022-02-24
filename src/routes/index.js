a09f66d33dbc66d2a903e4009e4c2f8d4ee41047
HamzaRizvi <hr04005@st.habib.edu.pk>
2022-02-24 19:47:06 +0500

    routes and sign in

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Routes, Route, Navigate } from 'react-router-dom';
 import PostLibrary from '../pages/PostLibrary/PostLibrary';
 import MediaLibrary from '../pages/MediaLibrary/MediaLibrary';
@@ -8,16 +8,40 @@ import SignIn from '../pages/SignIn/SignIn';
 import ViralLibrary from '../pages/ViralLibrary/ViralLibrary';
 
 const AppRoutes = () => {
+	const [loginData, setLoginData] = useState(
+		localStorage.getItem('user_data')
+			? JSON.parse(localStorage.getItem('user_data'))
+			: null
+	);
+
 	return (
 		<>
 			<Routes>
-				<Route path='/sign-in' element={<SignIn />} />
-				<Route path='/post-library' element={<PostLibrary />} />
-				<Route path='/media-library' element={<MediaLibrary />} />
-				<Route path='/quiz-library' element={<QuizLibrary />} />
-				<Route path='/top-banner' element={<TopBanner />} />
-				<Route path='/viral-library' element={<ViralLibrary />} />
-				<Route path='/' element={<Navigate replace to='/post-library' />} />
+				<Route
+					path='/sign-in'
+					element={<SignIn setLoginData={setLoginData} />}
+				/>
+				<Route
+					path='/post-library'
+					element={loginData ? <PostLibrary /> : <SignIn />}
+				/>
+				<Route
+					path='/media-library'
+					element={loginData ? <MediaLibrary /> : <SignIn />}
+				/>
+				<Route
+					path='/quiz-library'
+					element={loginData ? <QuizLibrary /> : <SignIn />}
+				/>
+				<Route
+					path='/top-banner'
+					element={loginData ? <TopBanner /> : <SignIn />}
+				/>
+				<Route
+					path='/viral-library'
+					element={loginData ? <ViralLibrary /> : <SignIn />}
+				/>
+				<Route path='/' element={<Navigate replace to='/sign-in' />} />
 			</Routes>
 		</>
 	);
