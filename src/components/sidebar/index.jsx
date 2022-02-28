2cf67c7f3799bc4159094e1e5d7e72d6a13d33b5
HamzaRizvi <hr04005@st.habib.edu.pk>
2022-02-28 14:15:49 +0500

    edit quiz and poll

--- src/components/sidebar/index.jsx
+++ src/components/sidebar/index.jsx
@@ -26,8 +26,6 @@ const Sidebar = () => {
 	const onLogoutSuccess = (res) => {
 		console.log('Logged out Success', res);
 		localStorage.removeItem('user_data');
-		alert('Logged out Successfully ✌');
-
 		navigate('/sign-in');
 	};
 
