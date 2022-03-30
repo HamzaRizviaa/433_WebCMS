33d7308d1e68ee44e11ae45a79a649297ba3a16d
KhadijaHussain3212 <khadija.hussain@cloudprimero.com>
2022-03-30 12:21:30 +0500

    sigin fixed

--- src/pages/SignIn/SignIn.jsx
+++ src/pages/SignIn/SignIn.jsx
@@ -18,11 +18,11 @@ const SignIn = ({ setLoginData }) => {
 	const [isLoadingSignIn, setIsLoadingSignin] = useState(false);
 	const [accessExpire, setAccessExpire] = useState(false);
 
-	useEffect(() => {
-		return () => {
-			setSignInError(false);
-		};
-	}, []);
+	// useEffect(() => {
+	// 	return () => {
+	// 		setSignInError(false);
+	// 	};
+	// }, []);
 
 	const navigate = useNavigate();
 
@@ -90,21 +90,20 @@ const SignIn = ({ setLoginData }) => {
 	}, [accessExpire]);
 
 	const handleLogin = async (googleData) => {
+		setIsLoadingSignin(true);
 		try {
-			setIsLoadingSignin(true);
 			const result = await axios.post(
 				`${process.env.REACT_APP_API_ENDPOINT}/cmsuser/verify-google-user`,
 				{
 					token: googleData.tokenId
 				}
 			);
-			if (
-				result?.data?.status_code === 200 &&
-				result?.data?.response === true
-			) {
+
+			if (result?.data?.status_code === 200) {
 				setLoginData(
 					localStorage.setItem('user_data', JSON.stringify(result?.data?.data))
 				);
+				// console.log(result, 'try - login result');
 
 				// setTimeout(() => {
 				// 	//remove localStorage data when token expires (12 hours) 43200000 - 12 hours , 3600000 -1 hour
@@ -114,19 +113,22 @@ const SignIn = ({ setLoginData }) => {
 				// }, [3600000]);
 
 				setAccessExpire(true);
-
 				setIsLoadingSignin(false);
 				//console.log(result?.data);
 				navigate('/post-library');
 				setSignInError(false);
 			}
 		} catch (e) {
+			// console.log('catch - login result');
 			setSignInError(true);
 			setIsLoadingSignin(false);
-
+			// setAccessExpire(false);
+			// setLoginData(null);
 			console.log(e, googleData.profileObj.email);
 		}
-
+		// console.log('out of catch - login result');
+		setSignInError(true);
+		setIsLoadingSignin(false);
 		// refreshTokenSetup(googleData);
 	};
 
