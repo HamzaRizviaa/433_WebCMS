789a20a4367b873b9c8b49c66fe2be68df708c74
aq2630 <a.qadirsscl@gmail.com>
2022-05-30 16:45:42 +0500

    fix: article builder and slider

---
+++ src/components/articleSlider/index.style.js
@@ -0,0 +1,48 @@
+/* eslint-disable no-dupe-keys */
+import { makeStyles } from '@material-ui/core';
+
+export const useStyles = makeStyles((theme) => ({
+	backdrop: {
+		zIndex: '1 !important',
+		color: 'rgba(0, 0, 0, 0.98) !important'
+	},
+
+	paper: {
+		color: `${theme.palette.neonYellow} !important`,
+		right: '0 !important',
+		borderLeft: `1px solid ${theme.palette.neonYellow} !important`,
+		height: '100vh !important',
+		zIndex: '5 !important',
+		position: 'fixed !important',
+		minWidth: 'calc(100% - 82px)',
+		transform: 'none !important',
+		overflowY: ' auto !important',
+		overflowX: 'hidden',
+		backgroundColor: `${theme.palette.black} !important`
+	},
+
+	content: {
+		padding: '4rem',
+		height: 'calc(100vh - 8rem)'
+	},
+
+	closeIcon: {
+		width: '3.2rem !important',
+		height: '3.2rem !important',
+		marginRight: '1.6rem !important',
+		padding: '0.5rem !important',
+		background: `${theme.palette.neonYellow} !important`,
+		borderRadius: '50% !important',
+		cursor: 'pointer !important'
+	},
+
+	header: {
+		display: 'flex',
+		alignItems: 'center',
+		marginBottom: '2.5rem'
+	},
+
+	heading: {
+		color: theme.palette.neonYellow
+	}
+}));
