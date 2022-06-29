6e3320c473b5afa2c723bd0b300efa287ef6c90d
HamzaRizviaa <hamzahassanrizvi@gmail.com>
2022-06-29 19:22:50 +0500

    news slide folder structure

---
+++ src/components/news/NewsSlide/index.jsx
@@ -0,0 +1,11 @@
+import React, { useState, useRef, useEffect } from 'react';
+import PropTypes from 'prop-types';
+import { Draggable } from 'react-beautiful-dnd';
+
+const NewsSlide = () => {
+	return <div>NewsSlide</div>;
+};
+
+NewsSlide.propTypes = {};
+
+export default NewsSlide;
