d3ba0d96a64306302636be26ddfd4f63929dc783
KhadijaHussain433 <khadija.hussain@by433.com>
2022-08-24 18:19:03 +0500

    delete and stop poll / quiz

--- src/pages/QuestionLibrary/questionLibraryService.js
+++ src/pages/QuestionLibrary/questionLibraryService.js
@@ -26,19 +26,19 @@ export default class questionLibraryService {
 		});
 	}
 
-	static getQuestionResultDetialApi(endPoint) {
-		return axios.get(`${process.env.REACT_APP_API_ENDPOINT}/${endPoint}`, {
-			headers: {
-				Authorization: `Bearer ${getLocalStorageDetails()?.access_token}`
-			}
-		});
-	}
+	// static getQuestionResultDetialApi(endPoint) {
+	// 	return axios.get(`${process.env.REACT_APP_API_ENDPOINT}/${endPoint}`, {
+	// 		headers: {
+	// 			Authorization: `Bearer ${getLocalStorageDetails()?.access_token}`
+	// 		}
+	// 	});
+	// }
 
-	static getQuestionResultParticipantApi(endPoint) {
-		return axios.get(`${process.env.REACT_APP_API_ENDPOINT}/${endPoint}`, {
-			headers: {
-				Authorization: `Bearer ${getLocalStorageDetails()?.access_token}`
-			}
-		});
-	}
+	// static getQuestionResultParticipantApi(endPoint) {
+	// 	return axios.get(`${process.env.REACT_APP_API_ENDPOINT}/${endPoint}`, {
+	// 		headers: {
+	// 			Authorization: `Bearer ${getLocalStorageDetails()?.access_token}`
+	// 		}
+	// 	});
+	// }
 }
