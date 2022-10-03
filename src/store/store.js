85cbe06800759c0a24743145879c17cf87320ccb
Abdul Qadir <45852281+aq2630@users.noreply.github.com>
2022-10-03 19:50:59 +0500

    Revert "Refactoring translations"

--- src/store/store.js
+++ src/store/store.js
@@ -1,5 +1,4 @@
 import { configureStore } from '@reduxjs/toolkit';
-import { setupListeners } from '@reduxjs/toolkit/query';
 import postLibraryReducer from '../pages/PostLibrary/postLibrarySlice';
 import mediaLibraryReducer from '../pages/MediaLibrary/mediaLibrarySlice';
 import questionLibraryReducer from '../pages/QuestionLibrary/questionLibrarySlice';
@@ -8,11 +7,8 @@ import articleLibraryReducer from '../pages/ArticleLibrary/articleLibrarySlice';
 import topBannerReducer from '../pages/TopBanner/topBannerSlice';
 import gamesLibraryReducer from '../pages/GamesLibrary/gamesLibrarySlice';
 import newsLibraryReducer from '../pages/NewsLibrary/newsLibrarySlice';
-import rootRtkQuery from '../features/rootRTKQuery';
-
 const store = configureStore({
 	reducer: {
-		[rootRtkQuery.reducerPath]: rootRtkQuery.reducer,
 		postLibrary: postLibraryReducer,
 		mediaLibraryOriginal: mediaLibraryReducer,
 		questionLibrary: questionLibraryReducer,
@@ -20,12 +16,8 @@ const store = configureStore({
 		ViralLibraryStore: viralLibraryReducer,
 		ArticleLibraryStore: articleLibraryReducer,
 		GamesLibraryStore: gamesLibraryReducer,
-		NewsLibrary: newsLibraryReducer,
-		//rtk root query
-	},
-	middleware: (getDefaultMiddleware) =>
-		getDefaultMiddleware().concat(rootRtkQuery.middleware)
+		NewsLibrary: newsLibraryReducer
+	}
 });
 
 export default store;
-setupListeners(store.dispatch);
