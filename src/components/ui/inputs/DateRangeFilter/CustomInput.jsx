43124090bf3eeacbd96a360297523877c8f34b42
Shariq Bin Shoaib <shariq.shoaib@by433.com>
2022-10-10 16:44:46 +0500

    fixed date format issue

--- src/components/ui/inputs/DateRangeFilter/CustomInput.jsx
+++ src/components/ui/inputs/DateRangeFilter/CustomInput.jsx
@@ -11,35 +11,44 @@ import { changeQueryParameters } from '../../../../data/utils/helper';
 const CustomInput = forwardRef(
 	({ onClick, startDate, endDate, isError }, ref) => {
 		const [searchParams, setSearchParams] = useSearchParams();
-		const formattedStartDate = dayjs(startDate).isValid()
-			? dayjs(startDate).format('MM-DD-YYYY')
-			: null;
-		const formattedEndDate = dayjs(endDate).isValid()
-			? dayjs(endDate).format('MM-DD-YYYY')
-			: null;
 
-		const queryParams = changeQueryParameters(searchParams, {
-			startDate: formattedStartDate,
-			endDate: formattedEndDate,
-			page: null
-		});
+		const handleClick = (e) => {
+			e.preventDefault();
+			e.stopPropagation();
+
+			const formattedStartDate = dayjs(startDate).isValid()
+				? dayjs(startDate).format('MM-DD-YYYY')
+				: null;
+			const formattedEndDate = dayjs(endDate).isValid()
+				? dayjs(endDate).format('MM-DD-YYYY')
+				: null;
+
+			const queryParams = changeQueryParameters(searchParams, {
+				startDate: formattedStartDate,
+				endDate: formattedEndDate,
+				page: null
+			});
+
+			setSearchParams(queryParams);
+		};
+
+		const startDateInput = dayjs(startDate).isValid()
+			? dayjs(startDate).format('DD-MM-YYYY')
+			: null;
+		const endDateInput = dayjs(endDate).isValid()
+			? dayjs(endDate).format('DD-MM-YYYY')
+			: null;
 
 		const classes = useStyles({ hasData: !!startDate || !!endDate, isError });
 
 		return (
 			<div className={classes.customDateInput} onClick={onClick} ref={ref}>
 				<span className={classes.inputField}>
-					{formattedStartDate || 'Start Date'} <ArrowForwardIosIcon />
-					{formattedEndDate || 'End Date'}
+					{startDateInput || 'Start Date'} <ArrowForwardIosIcon />
+					{endDateInput || 'End Date'}
 				</span>
 				<span className={classes.inputIcon}>
-					<Calendar
-						onClick={(e) => {
-							e.preventDefault();
-							e.stopPropagation();
-							setSearchParams(queryParams);
-						}}
-					/>
+					<Calendar onClick={handleClick} />
 				</span>
 			</div>
 		);
