b0f8a0b38ab56ced74c32d09f07e7167636a94d0
MustafaNaqvi12 <mustafa.imam@by433.com>
2022-10-25 14:16:20 +0500

    CMS-1690 removing comments

--- src/components/ui/inputs/RichTextEditor/index.style.js
+++ src/components/ui/inputs/RichTextEditor/index.style.js
@@ -4,18 +4,12 @@ export const useTextEditorStyles = makeStyles(() => ({
 	editor: {
 		margin: '10px 0px',
 		'& > div': {
-			//tox tox-tinymce tox-platform-touch
 			border: `1px solid #404040 !important`,
 			borderRadius: '8px',
-			//top bars and middle box
 			'& > div.tox-editor-container': {
-				//tox-editor-container
 				backgroundColor: '#404040 !important',
-				// top bars
 				'& > div.tox-editor-header': {
-					//tox-editor-header
 					'& > div.tox-menubar': {
-						//tox-menubar
 						backgroundColor: `#404040 !important`,
 						background: `#404040 !important`,
 						'& > button.tox-mbtn': {
@@ -32,7 +26,7 @@ export const useTextEditorStyles = makeStyles(() => ({
 						}
 					},
 					'& > div.tox-toolbar': {
-						display: 'none !important', // scrolling second tool bar line
+						display: 'none !important',
 						backgroundColor: `#404040 !important`,
 						background: `#404040 !important`,
 						'&  >div.tox-toolbar__group': {
@@ -71,57 +65,18 @@ export const useTextEditorStyles = makeStyles(() => ({
 					},
 					'& > div.tox-toolbar-overlord': {
 						display: 'none !important'
-						//tox-toolbar tox-toolbar--scrolling
-						// '& > div.tox-toolbar__primary': {
-						// 	//tox-toolbar__group
-						// 	borderTop: `1px solid #404040 !important`,
-						// 	backgroundColor: `#404040 !important`,
-						// 	// border-right: none !important;
-						// 	'& > div > button.tox-tbtn': {
-						// 		// border-right: 1px solid white !important;
-						// 		'& > span.tox-icon': {
-						// 			color: ` white !important`,
-						// 			'& > svg': {
-						// 				fill: `white !important`
-						// 			}
-						// 		},
-						// 		'&:focus': {
-						// 			background: `#404040 !important`
-						// 		},
-						// 		'&:hover': {
-						// 			background: `#404040 !important`
-						// 		},
-						// 		'&:active': {
-						// 			background: `#404040 !important`
-						// 		}
-						// 	}
-						// }
 					}
 				},
-				//middle text area
 				'& > div.tox-sidebar-wrap': {
-					//tox-sidebar-wrap
 					'& > div.tox-edit-area': {
 						'& > iframe.tox-edit-area__iframe': {
 							backgroundColor: `black !important`
-							// color: white !important ;
-							// #document {
-							//  color: white !important;
-							// }
-							// > html {
-							//  color: white !important;
-							// }
-							// .mce-content-body {
-							//  color: white !important;
-							// }
 						}
 					}
 				}
 			},
-			//bottom bar
 			'& > div.tox-statusbar': {
 				background: `#404040 !important`,
-				//tox-statusbar
 				'& > div.tox-statusbar__text-container': {
 					'& > div.tox-statusbar__path': {
 						'& > div.tox-statusbar__path-item': {
@@ -133,9 +88,6 @@ export const useTextEditorStyles = makeStyles(() => ({
 					}
 				}
 			}
-			// '& >div .tox-menu': {
-			// 	backgroundColor: '#404040 !important'
-			// }
 		}
 	}
 }));
