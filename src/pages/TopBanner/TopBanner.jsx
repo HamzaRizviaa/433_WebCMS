fc02e5d26ae9ac5d5832c0c3b5b26ca2f51afe1e
HamzaRizvia <hamza.rizvi@by433.com>
2022-11-10 18:18:27 +0500

    CMS-1700 loader on banners

--- src/pages/TopBanner/TopBanner.jsx
+++ src/pages/TopBanner/TopBanner.jsx
@@ -1,14 +1,7 @@
-/* eslint-disable no-unused-vars */
-/* eslint-disable react/prop-types */
-/* eslint-disable react/display-name */
 import React from 'react';
-import TabsUnstyled from '@mui/base/TabsUnstyled';
-import TabsListUnstyled from '@mui/base/TabsListUnstyled';
-import TabPanelUnstyled from '@mui/base/TabPanelUnstyled';
-import TabUnstyled from '@mui/base/TabUnstyled';
-import { useStyles } from './topBanner';
-import BannerForm from '../../components/forms/BannerForm';
 import { useSelector } from 'react-redux';
+import { selectBannerStatus } from '../../data/selectors';
+import BannerForm from '../../components/forms/BannerForm';
 import DashboardLayout from '../../components/layouts/DashboardLayout';
 import TabPanes from '../../components/ui/TabPanes';
 
@@ -16,20 +9,13 @@ import TabPanes from '../../components/ui/TabPanes';
  * @component
  */
 const TopBanner = () => {
-	const muiClasses = useStyles();
 	const getBannerContentState = useSelector(
 		(state) => state.rootReducer.topBanner.getBannerContentState
 	);
-	const bannerState = useSelector(
-		(state) => state.rootReducer.topBanner.getBannerStatus
-	);
+
+	const bannerStatus = useSelector(selectBannerStatus);
 
 	const headings = ['Home', 'Media'];
-
-	// const options = [
-	// 	{ value: 0, label: 'Home' },
-	// 	{ value: 1, label: 'Media' }
-	// ];
 
 	return (
 		<DashboardLayout
@@ -38,10 +24,11 @@ const TopBanner = () => {
 			hideSearchFilter
 			hideDateFilter
 			hideLibraryText
+			isLoading={bannerStatus === 'loading'}
 		>
 			<TabPanes
 				headings={headings}
-				disabled={getBannerContentState && bannerState ? false : true}
+				disabled={getBannerContentState && bannerStatus ? false : true}
 			>
 				<TabPanes.TabPanel value={0}>
 					<BannerForm tabValue={'home'} />
