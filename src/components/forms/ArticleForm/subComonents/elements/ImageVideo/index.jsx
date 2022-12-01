31f7790eac855cfffc16ca102dc88c0a6fcb53b0
HamzaRizvia <hamza.rizvi@by433.com>
2022-12-01 14:06:32 +0500

    image video element start

---
+++ src/components/forms/ArticleForm/subComonents/elements/ImageVideo/index.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import DraggableLayoutWrapper from '../../../../../layouts/DraggableLayoutWrapper';
+import DraggableCardLayout from '../../../../../layouts/DraggableCardLayout';
+
+const ImageVideo = () => {
+	return (
+		<DraggableLayoutWrapper>
+			<DraggableCardLayout></DraggableCardLayout>
+		</DraggableLayoutWrapper>
+	);
+};
+
+export default ImageVideo;
