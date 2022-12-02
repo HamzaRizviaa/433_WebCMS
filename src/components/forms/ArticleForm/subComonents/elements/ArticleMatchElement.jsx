54ec8982d28c0c3223aa035b433c465cdfaf8efd
MustafaNaqvi12 <mustafa.imam@by433.com>
2022-12-02 21:53:23 +0500

    CMS-1913 article match element

---
+++ src/components/forms/ArticleForm/subComonents/elements/ArticleMatchElement.jsx
@@ -0,0 +1,63 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import FormikSelect from '../../../../ui/inputs/formik/FormikSelect';
+import { useState } from 'react';
+import { getLeagueOptions, getTeamOptions, getMatchName } from '../../../../../data/utils';
+
+const ArticleMatchElement = ({ initialData, data, isPublished }) => {
+	const [leagues, setLeagues] = useState([]);
+    const [teams, setTeams] = useState([])
+    const [matches, setMatches] = useState([])
+
+    useEffect(() => {
+		if (isPublished) {
+			setLeagues([
+				{ name: initialData?.league_name, id: initialData?.league_name }
+			]);
+			return;
+		}
+		data && setLeagues(data);
+	}, [data]);
+
+    const handleLeagueChange = (val) => {
+        const leagueTeams = getTeamOptions(leagues, val)
+        setTeams(leagueTeams)
+    }
+    const handleTeamChange = (val) => {
+        const teamMatches = teams.map((value) => value.name === val.name)?.matches
+        teamMatches.map((match) => {
+            match.name = getMatchName(match.startdate, match.name)
+        })
+        setMatches(teamMatches)
+    }
+
+	return (
+		<div>
+			<FormikSelect
+				name={`elements.${index}.league`}
+				placeholder='SELECT LEAGUE'
+				options={getLeagueOptions(leagues)}
+                onChange={handleLeagueChange}
+			/>
+			<FormikSelect 
+                name={`elements.${index}.team`} 
+                placeholder='SELECT TEAM' 
+                options={teams}
+                onChange={handleTeamChange}
+            />
+			<FormikSelect 
+                name={`elements.${index}.match`} 
+                placeholder='SELECT MATCH'
+                options={matches}
+            />
+		</div>
+	);
+};
+
+ArticleMatchElement.propTypes = {
+    initialData: PropTypes.object,
+    data: PropTypes.array,
+    isPublished: PropTypes.bool
+}
+
+export default ArticleMatchElement;
