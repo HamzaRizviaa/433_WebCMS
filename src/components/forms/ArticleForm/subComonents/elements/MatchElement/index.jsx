58f32e5e27cba594483bb30ef7344da4de344dab
MustafaNaqvi12 <mustafa.imam@by433.com>
2022-12-05 15:06:08 +0500

    CMS-1913 formatting

--- src/components/forms/ArticleForm/subComonents/elements/MatchElement/index.jsx
+++ src/components/forms/ArticleForm/subComonents/elements/MatchElement/index.jsx
@@ -2,14 +2,18 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import FormikSelect from '../../../../../ui/inputs/formik/FormikSelect';
 import { useState } from 'react';
-import { getLeagueOptions, getTeamOptions, getMatchName } from '../../../../../../data/utils';
+import {
+	getLeagueOptions,
+	getTeamOptions,
+	getMatchName
+} from '../../../../../../data/utils';
 
 const ArticleMatchElement = ({ index, initialData, data, isPublished }) => {
 	const [leagues, setLeagues] = useState([]);
-    const [teams, setTeams] = useState([])
-    const [matches, setMatches] = useState([])
+	const [teams, setTeams] = useState([]);
+	const [matches, setMatches] = useState([]);
 
-    useEffect(() => {
+	useEffect(() => {
 		if (isPublished) {
 			setLeagues([
 				{ name: initialData?.league_name, id: initialData?.league_name }
@@ -19,17 +23,17 @@ const ArticleMatchElement = ({ index, initialData, data, isPublished }) => {
 		data && setLeagues(data);
 	}, [data]);
 
-    const handleLeagueChange = (val) => {
-        const leagueTeams = getTeamOptions(leagues, val)
-        setTeams(leagueTeams)
-    }
-    const handleTeamChange = (val) => {
-        const teamMatches = teams.map((value) => value.name === val.name)?.matches
-        teamMatches.map((match) => {
-            match.name = getMatchName(match.startdate, match.name)
-        })
-        setMatches(teamMatches)
-    }
+	const handleLeagueChange = (val) => {
+		const leagueTeams = getTeamOptions(leagues, val);
+		setTeams(leagueTeams);
+	};
+	const handleTeamChange = (val) => {
+		const teamMatches = teams.map((value) => value.name === val.name)?.matches;
+		teamMatches.map((match) => {
+			match.name = getMatchName(match.startdate, match.name);
+		});
+		setMatches(teamMatches);
+	};
 
 	return (
 		<div>
@@ -37,31 +41,31 @@ const ArticleMatchElement = ({ index, initialData, data, isPublished }) => {
 				name={`elements.${index}.league`}
 				placeholder='SELECT LEAGUE'
 				options={getLeagueOptions(leagues)}
-                disabled={isPublished}
-                onChange={handleLeagueChange}
+				disabled={isPublished}
+				onChange={handleLeagueChange}
 			/>
-			<FormikSelect 
-                name={`elements.${index}.team`} 
-                placeholder='SELECT TEAM' 
-                options={teams}
-                disabled={isPublished || teams.length === 0}
-                onChange={handleTeamChange}
-            />
-			<FormikSelect 
-                name={`elements.${index}.match`} 
-                placeholder='SELECT MATCH'
-                options={matches}
-                disabled={isPublished || matches.length === 0}
-            />
+			<FormikSelect
+				name={`elements.${index}.team`}
+				placeholder='SELECT TEAM'
+				options={teams}
+				disabled={isPublished || teams.length === 0}
+				onChange={handleTeamChange}
+			/>
+			<FormikSelect
+				name={`elements.${index}.match`}
+				placeholder='SELECT MATCH'
+				options={matches}
+				disabled={isPublished || matches.length === 0}
+			/>
 		</div>
 	);
 };
 
 ArticleMatchElement.propTypes = {
-    index: PropTypes.number.isRequired,
-    initialData: PropTypes.object,
-    data: PropTypes.array,
-    isPublished: PropTypes.bool
-}
+	index: PropTypes.number.isRequired,
+	initialData: PropTypes.object,
+	data: PropTypes.array,
+	isPublished: PropTypes.bool
+};
 
 export default ArticleMatchElement;
