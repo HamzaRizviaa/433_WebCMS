a6a958f9fc0387d1e398daa0cb1068b0b8731318
hamzakhan <hamza.khan@by433.com>
2022-12-08 19:31:03 +0500

    articke services and actions

--- src/data/services/articleLibraryService.js
+++ src/data/services/articleLibraryService.js
@@ -30,6 +30,27 @@ class ArticleLibraryService {
 
 	static getArticleSubCategoriesApi = (id) =>
 		axiosInstance.get(`/article/get-sub-categories/${id}`);
+
+	static postArticle = (data, apiVersion = 2) => {
+		return axiosInstance.post(`/article/post-article`, data, {
+			params: {
+				api_version: apiVersion
+			}
+		});
+	};
+
+	/**
+	 * This function is responsible for the deletion of a single article
+	 * @param {Object} data - The data field contains the article_id and is_draft properties
+	 * @returns Promise of the AxiosResponse Object
+	 */
+	static deleteArticle = (data) => {
+		return axiosInstance.post(`/article/delete-article`, data);
+	};
+
+	static getArticleCheckTitle = (title) => {
+		return axiosInstance.get(`/article/check/${title}`);
+	};
 }
 
 export default ArticleLibraryService;
