aadbf2e3f9ef2285d970956e4f512bfb23bfbcb7
HamzaRizvia <hamza.rizvi@by433.com>
2022-12-14 07:12:24 +0500

    Settings Layout

--- src/components/layouts/SettingsLayout/index.jsx
+++ src/components/layouts/SettingsLayout/index.jsx
@@ -1,10 +1,17 @@
-/* eslint-disable no-unused-vars */
 import React from 'react';
 import { PropTypes } from 'prop-types';
 import { useSettingsLayoutStyles } from './index.style';
+import { Typography } from '@material-ui/core';
 
 const SettingsLayout = ({ title, children }) => {
-	return <div>SettingsLayout</div>;
+	const classes = useSettingsLayoutStyles();
+
+	return (
+		<div className={classes.settingsLayoutWrapper}>
+			<Typography className={classes.title}>{title}</Typography>
+			{children}
+		</div>
+	);
 };
 
 SettingsLayout.propTypes = {
--- src/components/layouts/SettingsLayout/index.style.js
+++ src/components/layouts/SettingsLayout/index.style.js
@@ -1,3 +1,21 @@
 import { makeStyles } from '@material-ui/core';
 
-export const useSettingsLayoutStyles = makeStyles(() => ({}));
+export const useSettingsLayoutStyles = makeStyles((theme) => ({
+	settingsLayoutWrapper: {
+		// display: 'flex',
+		// alignItems: 'flex-start',
+		// flexDirection: 'column',
+		backgroundColor: theme.palette.primary.main,
+		border: `1px solid ${theme.palette.black80}`,
+		borderRadius: '8px',
+		padding: '24px',
+		margin: '20px 0px',
+		gap: '16px'
+	},
+	title: {
+		fontSize: '16px',
+		lineHeight: '24px',
+		fontWeight: '700',
+		marginBottom: '4px'
+	}
+}));
