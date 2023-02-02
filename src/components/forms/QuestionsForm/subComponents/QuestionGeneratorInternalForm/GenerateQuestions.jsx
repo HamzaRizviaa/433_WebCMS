3ab20df16556eba9c6b4f76723772daa39b46082
Hammad-6304 <103407647+hammad-6304@users.noreply.github.com>
2023-02-02 17:01:24 +0500

    replace formik select field

--- src/components/forms/QuestionsForm/subComponents/QuestionGeneratorInternalForm/GenerateQuestions.jsx
+++ src/components/forms/QuestionsForm/subComponents/QuestionGeneratorInternalForm/GenerateQuestions.jsx
@@ -1,11 +1,46 @@
 import React from 'react';
+import { useEffect } from 'react';
+import { useState } from 'react';
 import Button from '../../../../ui/Button';
-import FormikSelect from '../../../../ui/inputs/formik/FormikSelect';
+// import FormikSelect from '../../../../ui/inputs/formik/FormikSelect';
 import RadioButton from '../../../../ui/inputs/RadioButton';
+import SelectField from '../../../../ui/inputs/SelectField';
 // import PropTypes from 'prop-types';
 import { useQuestionsStyles } from '../../index.style';
 
+const staticOptions = [
+	{ name: 'random', key: 1 },
+	{ name: 'ronaldo', key: 2 }
+];
+
 const GenerateQuestions = () => {
+	// controlled fields state
+	const [values, setValues] = useState({
+		league: 'random',
+		team: 'random',
+		year: 'random',
+		stat: 'random'
+	});
+
+	const { league, team, year, stat } = values;
+
+	/**
+	 * Hooks
+	 */
+
+	useEffect(() => {
+		console.log('value changed', values);
+	}, [league, team, year, stat]);
+
+	/**
+	 * Methods
+	 */
+
+	const handleValueChange = (value, name) => {
+		setValues({ ...values, [name]: value });
+	};
+
+	// stylings
 	const classes = useQuestionsStyles();
 	return (
 		<div>
@@ -27,35 +62,47 @@ const GenerateQuestions = () => {
 				{/* Filter Dropdown Fields */}
 				<div>
 					<div className={classes.filterField}>
-						<FormikSelect
+						<SelectField
 							name='league'
 							label='SELECT LEAGUE'
-							options={[]}
+							options={staticOptions}
+							mapOptions={{ valueKey: 'name', labelKey: 'name' }}
 							placeholder='Please Select'
+							value={values.league}
+							onChange={(value) => handleValueChange(value, 'league')}
 						/>
 					</div>
 					<div className={classes.filterField}>
-						<FormikSelect
+						<SelectField
 							name='team'
 							label='SELECT TEAM'
-							options={[]}
+							options={staticOptions}
+							mapOptions={{ valueKey: 'name', labelKey: 'name' }}
 							placeholder='Please Select'
+							value={values.team}
+							onChange={(value) => handleValueChange(value, 'team')}
 						/>{' '}
 					</div>
 					<div className={classes.filterField}>
-						<FormikSelect
+						<SelectField
 							name='year'
 							label='SELECT YEAR'
-							options={[]}
+							options={staticOptions}
+							mapOptions={{ valueKey: 'name', labelKey: 'name' }}
 							placeholder='Please Select'
+							value={values.year}
+							onChange={(value) => handleValueChange(value, 'year')}
 						/>{' '}
 					</div>
 					<div className={classes.filterField}>
-						<FormikSelect
+						<SelectField
 							name='stat'
 							label='SELECT STAT'
-							options={[]}
+							options={staticOptions}
+							mapOptions={{ valueKey: 'name', labelKey: 'name' }}
 							placeholder='Please Select'
+							value={values.stat}
+							onChange={(value) => handleValueChange(value, 'stat')}
 						/>{' '}
 					</div>
 				</div>
