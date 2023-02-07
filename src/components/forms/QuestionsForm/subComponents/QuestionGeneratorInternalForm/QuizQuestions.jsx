eb8d3b86874ce02c8db1d2333a651932b93b1fa3
HamzaRizvia <hamza.rizvi@by433.com>
2023-02-07 20:37:52 +0500

    save draft button disabled

--- src/components/forms/QuestionsForm/subComponents/QuestionGeneratorInternalForm/QuizQuestions.jsx
+++ src/components/forms/QuestionsForm/subComponents/QuestionGeneratorInternalForm/QuizQuestions.jsx
@@ -1,20 +1,38 @@
-import React from 'react';
+import React, { useMemo } from 'react';
 import PropTypes from 'prop-types';
 import { useFormikContext } from 'formik';
 import { useQuestionsStyles } from '../../index.style';
 import { EmptyQuizQuestions } from '../../../../../assets/svg-icons';
 import QuizQuestionsForm from './QuizQuestionsForm';
 import Button from '../../../../ui/Button';
+import { isEqual, omit, pick } from 'lodash';
+import {
+	areAllFieldsEmpty,
+	questionsFormInitialValues
+} from '../../../../../data/helpers';
 
 const QuizQuestions = ({ onDraftClick }) => {
 	const classes = useQuestionsStyles();
-	const { values } = useFormikContext();
+	const { values, dirty } = useFormikContext();
+
+	const isDraftDisabled = useMemo(() => {
+		const isAnyQuestionSlideEmpty = values.questions.some((item) =>
+			areAllFieldsEmpty(omit(item, ['answers']))
+		);
+
+		const isEqualToDefaultValues = isEqual(
+			pick(values, Object.keys(questionsFormInitialValues([], true))),
+			questionsFormInitialValues([], true)
+		);
+
+		return !dirty || isAnyQuestionSlideEmpty || isEqualToDefaultValues;
+	}, [values, dirty]);
 
 	return (
 		<div>
 			<h2 className={classes.quizTitle}>Quiz</h2>
 
-			{!values.questions.length  ? (
+			{!values.questions.length ? (
 				<div className={classes.emptyQuizInfoPage}>
 					<EmptyQuizQuestions />
 					<p className={classes.emptyQuizInfoText}>
@@ -30,6 +48,7 @@ const QuizQuestions = ({ onDraftClick }) => {
 					<Button
 						fullWidth
 						size='large'
+						disabled={isDraftDisabled}
 						variant='outlined'
 						onClick={onDraftClick}
 						className={classes.quizQuestionsDraftButtonWrapper}
