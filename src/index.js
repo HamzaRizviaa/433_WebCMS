9ab641c536259a1182ad6af00c1ab926eb99422e
Shariq Bin Shoaib <shariq.shoaib@by433.com>
2023-02-17 19:23:07 +0500

    implemented google oauth new library

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { BrowserRouter as Router } from 'react-router-dom';
 import { ToastContainer } from 'react-toastify';
+import { GoogleOAuthProvider } from '@react-oauth/google';
 import store from './data/store';
 import AppRoutes from './routes';
 
@@ -10,11 +11,13 @@ import './styles/index.scss';
 import 'react-toastify/dist/ReactToastify.css';
 
 ReactDOM.render(
-	<Provider store={store}>
-		<Router>
-			<AppRoutes />
-			<ToastContainer />
-		</Router>
-	</Provider>,
+	<GoogleOAuthProvider clientId='761006834675-0717aiakfe9at8d7jahf10hdgevu7acg.apps.googleusercontent.com'>
+		<Provider store={store}>
+			<Router>
+				<AppRoutes />
+				<ToastContainer />
+			</Router>
+		</Provider>
+	</GoogleOAuthProvider>,
 	document.getElementById('root')
 );
