d469329273aacf4d61ab74c29332104eb79324aa
HamzaRizvia <hamza.rizvi@by433.com>
2023-02-24 07:21:32 +0500

    unauthorized page

--- src/pages/Unauthorized/index.jsx
+++ src/pages/Unauthorized/index.jsx
@@ -1,9 +1,25 @@
 import React from 'react';
+import { unauthorizedStyles } from './Unauthorized.style';
+import BackgroundGIF from '../../assets/you shall not pass.gif';
+import { DeniedError } from '../../assets/svg-icons';
 
 const Unauthorized = () => {
+	const classes = unauthorizedStyles();
 	return (
 		<>
-			<h1>dawdaw</h1>
+			<div className={classes.wrapper}>
+				<img className={classes.gif} src={BackgroundGIF} />
+				<div className={classes.modal}>
+					<span className={classes.deniedSpan}>
+						<DeniedError className={classes.deniedIcon} />
+						Access Denied
+					</span>
+					<p>
+						You don’t have permission to access this page. Please contact an
+						Admin to grant you access
+					</p>
+				</div>
+			</div>
 		</>
 	);
 };
